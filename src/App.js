060370146d7337a56f1f4fb70de8d0216222ec66
Ayush Srivastava <ayushs.1992@gmail.com>
2020-09-22 22:44:51 +0530

    Remove comments, Added route icons, logo.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "./components/drawer";
 import routes from "./routes.config.json";
@@ -9,22 +10,42 @@ import "./styles.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex"
+    display: "flex",
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3)
+    padding: theme.spacing(3),
   },
   pane: {
-    flex: 1
-  }
+    flex: 1,
+  },
+  logoContainer: {
+    display: "flex",
+    height: "100px",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
 }));
+
+const logo = (classes) => (
+  <div className={classes.logoContainer}>
+    <Avatar
+      className={classes.large}
+      alt="logo"
+      src="https://avatars0.githubusercontent.com/u/71497567?s=400&u=92109698dfd9854b098c6f6bff447bab49f705b5&v=4"
+    />
+  </div>
+);
 
 export default function App({ children }) {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Drawer routes={routes} disableParentLink />
+      <Drawer routes={routes} disableParentLink logo={logo(classes)} />
       <div className={classes.pane}>
         <AppBar position="static">
           <Toolbar>
--- src/components/drawer/drawer.jsx
+++ src/components/drawer/drawer.jsx
@@ -7,64 +7,60 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
-// import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-// import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import { Link } from "react-router-dom";
 import List from "@material-ui/core/List";
 import Icon from "@material-ui/core/Icon";
-// import ExpandLess from "@material-ui/icons/ExpandLess";
-// import ExpandMore from "@material-ui/icons/ExpandMore";
 import Collapse from "@material-ui/core/Collapse";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex"
+    display: "flex",
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
+      duration: theme.transitions.duration.leavingScreen,
+    }),
   },
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   menuButton: {
-    marginRight: 36
+    marginRight: 36,
   },
   hide: {
-    display: "none"
+    display: "none",
   },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    whiteSpace: "nowrap"
+    whiteSpace: "nowrap",
   },
   drawerOpen: {
     width: drawerWidth,
     transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   drawerClose: {
     transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
+      duration: theme.transitions.duration.leavingScreen,
     }),
     overflowX: "hidden",
     width: theme.spacing(7) + 1,
     [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9) + 1
-    }
+      width: theme.spacing(9) + 1,
+    },
   },
   toolbar: {
     display: "flex",
@@ -75,15 +71,15 @@ const useStyles = makeStyles((theme) => ({
     bottom: 0,
     width: "100%",
     // necessary for content to be below app bar
-    ...theme.mixins.toolbar
+    ...theme.mixins.toolbar,
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3)
+    padding: theme.spacing(3),
   },
   nested: {
-    paddingLeft: theme.spacing(10)
-  }
+    paddingLeft: theme.spacing(10),
+  },
 }));
 
 function SubMenu({ route, classes, disableParentLink }) {
@@ -95,7 +91,7 @@ function SubMenu({ route, classes, disableParentLink }) {
       : {
           component: Link,
           to: route.route,
-          value: route.route
+          value: route.route,
         };
 
   return (
@@ -130,7 +126,7 @@ function SubMenu({ route, classes, disableParentLink }) {
   );
 }
 
-function CustomDrawer({ routes = [], disableParentLink = false }) {
+function CustomDrawer({ routes = [], logo, disableParentLink = false }) {
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
 
@@ -141,15 +137,16 @@ function CustomDrawer({ routes = [], disableParentLink = false }) {
       variant="permanent"
       className={clsx(classes.drawer, {
         [classes.drawerOpen]: open,
-        [classes.drawerClose]: !open
+        [classes.drawerClose]: !open,
       })}
       classes={{
         paper: clsx({
           [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open
-        })
+          [classes.drawerClose]: !open,
+        }),
       }}
     >
+      {logo}
       <Divider />
       <List>
         {routes.map((route, index) => (
