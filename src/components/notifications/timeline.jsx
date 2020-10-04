d7837c6cefb09c799564d9785b0c441e6ab447b1
Ayush Srivastava <ayushs.1992@gmail.com>
2020-10-05 00:37:24 +0530

    Added more content to timeline.

--- src/components/notifications/timeline.jsx
+++ src/components/notifications/timeline.jsx
@@ -8,15 +8,22 @@ import TimelineContent from "@material-ui/lab/TimelineContent";
 import TimelineOppositeContent from "@material-ui/lab/TimelineOppositeContent";
 import TimelineDot from "@material-ui/lab/TimelineDot";
 import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import AvatarGroup from "@material-ui/lab/AvatarGroup";
 
 const useStyles = makeStyles(() => ({
   oppositeContent: {
     // TODO: adjust this value accordingly
     flex: 0.2,
   },
-  timelineContent: {
-    padding: "12px 16px",
+  heading: {
+    fontWeight: "bolder",
+    marginBottom: "5px",
   },
+  avatarGroup: {
+    alignItems: "center",
+    justifyContent: "flex-end" 
+  }
 }));
 
 export default function NotificationTimeline() {
@@ -36,11 +43,11 @@ export default function NotificationTimeline() {
           <TimelineDot variant="outlined" />
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent className={classes.timelineContent}>
-          <Typography variant="h5" component="h1">
-            Code
+        <TimelineContent>
+          <Typography variant="h5" component="h1" className={classes.heading}>
+            India Wins World Cup
           </Typography>
-          <Typography>Because it&apos;s awesome!</Typography>
+          <Typography>Dhoni's Heroic wins india world cup.</Typography>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
@@ -56,11 +63,33 @@ export default function NotificationTimeline() {
           <TimelineDot variant="outlined" />
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent className={classes.timelineContent}>
-          <Typography variant="h5" component="h1">
-            Code
+        <TimelineContent>
+          <Typography variant="h5" component="h1" className={classes.heading}>
+            Election 2020
           </Typography>
-          <Typography>Because it&apos;s awesome!</Typography>
+          <Typography>U.S.A to vote for elections today.</Typography>
+          <AvatarGroup max={4} className={classes.avatarGroup}>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://next--material-ui.netlify.app/static/images/avatar/1.jpg"
+            />
+            <Avatar
+              alt="Travis Howard"
+              src="https://next--material-ui.netlify.app/static/images/avatar/2.jpg"
+            />
+            <Avatar
+              alt="Cindy Baker"
+              src="https://next--material-ui.netlify.app/static/images/avatar/3.jpg"
+            />
+            <Avatar
+              alt="Agnes Walker"
+              src="https://next--material-ui.netlify.app/static/images/avatar/4.jpg"
+            />
+            <Avatar
+              alt="Trevor Henderson"
+              src="https://next--material-ui.netlify.app/static/images/avatar/5.jpg"
+            />
+          </AvatarGroup>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
@@ -75,8 +104,8 @@ export default function NotificationTimeline() {
         <TimelineSeparator>
           <TimelineDot variant="outlined" />
         </TimelineSeparator>
-        <TimelineContent className={classes.timelineContent}>
-          <Typography variant="h5" component="h1">
+        <TimelineContent>
+          <Typography variant="h5" component="h1" className={classes.heading}>
             Code
           </Typography>
           <Typography>Because it&apos;s awesome!</Typography>
